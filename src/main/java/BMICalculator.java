74911dfdfe72014d8f4a366f4a840de0f9fbf991
mi-uhu <michael.unterhuber94@gmail.com>
2021-11-06 09:21:41 +0100

    implemented class BMICalculator

--- src/main/java/BMICalculator.java
+++ src/main/java/BMICalculator.java
@@ -8,36 +8,102 @@ Matrikel-Number: 01609582
  */
 
 
-
 public class BMICalculator {
 
-    private String firstname;
-    private String lastname;
-    private char gender;
+    private final String firstname;
+    private final String lastname;
+    private final char gender;
     private int bodyHeight;
     private double bodyWeight;
 
-    // TODO: Implement Constructor
+    public BMICalculator(String firstname, String lastname, char gender, int bodyHeight, double bodyWeight) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.bodyHeight = bodyHeight;
+        this.bodyWeight = bodyWeight;
+    }
 
-    // TODO: Implement getter/setter
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getBodyHeight() {
+        return bodyHeight;
+    }
+
+    public void setBodyHeight(int bodyHeight) {
+        this.bodyHeight = bodyHeight;
+    }
+
+    public double getBodyWeight() {
+        return bodyWeight;
+    }
+
+    public void setBodyWeight(double bodyWeight) {
+        this.bodyWeight = bodyWeight;
+    }
 
     private double round(double value) {
         return Math.round(value * 100) / 100D;
     }
 
     public double calculateBMI() {
-        // TODO: Your implementation
-        return 0;
+        return bodyWeight / (((double)bodyHeight / 100) * ((double)bodyHeight / 100));
     }
 
     public int calculateBMICategory() {
-        // TODO: Your implementation
-        return 0;
+        double bmi = calculateBMI();
+
+        if (gender == 'm') {
+            if (bmi < 16.0)
+                return -2;
+            else if (bmi < 18.5)
+                return -1;
+            else if (bmi < 25)
+                return 0;
+            else if (bmi < 35)
+                return 1;
+            else
+                return 2;
+        } else {
+            if (bmi < 15.0)
+                return -2;
+            else if (bmi < 17.5)
+                return -1;
+            else if (bmi < 24)
+                return 0;
+            else if (bmi < 34)
+                return 1;
+            else
+                return 2;
+        }
     }
 
     public String getBMICategoryName() {
-        // TODO: Your implementation
-        return "";
+        int category = calculateBMICategory();
+        switch (category){
+            case -2:
+                return "Sehr starkes Untergewicht";
+            case -1:
+                return "Untergewicht";
+            case 0:
+                return "Normalgewicht";
+            case 1:
+                return "Übergewicht";
+            case 2:
+                return "Sehr starkes Übergewicht";
+            default:
+                throw new IllegalArgumentException("Invalid bmi category");
+        }
     }
 
 }
